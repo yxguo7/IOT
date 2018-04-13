c3a558d7f95aa63cc159e4b3e9376d8ec4b69f68
yxguo7 <32585152+yxguo7@users.noreply.github.com>
2018-04-12 23:51:01 -0700

    ac02 final

--- led_gui.py
+++ led_gui.py
@@ -16,44 +16,75 @@ def exitProgram():
     digitalWrite(myLED3, 0)
     app.destroy()
 
-def sliderToggleLED(led, slider_value):
-    analogWrite(led, int(slider_value))
+def sliderToggleLED1(slider_value):
+    analogWrite(myLED1, int(slider_value))
 
-def ledToggle(led, button, text):
-    myLED = led
-    ledButton = button
-    textLED = text
-    button_status = digitalRead(myLED)
+def sliderToggleLED2(slider_value):
+    analogWrite(myLED2, int(slider_value))
+
+def sliderToggleLED3(slider_value):
+    analogWrite(myLED3, int(slider_value))
+
+def ledToggle1():
+    button_status = digitalRead(myLED1)
     if (button_status == 0):
-        digitalWrite(myLED, 1)
-        ledButton.text = "Turn OFF"
-        ledButton.bg = "green"
-        ledButton.text_color = "white"
-        textLED.text_color = "green"
+        digitalWrite(myLED1, 1)
+        ledButton1.text = "Turn OFF"
+        ledButton1.bg = "green"
+        ledButton1.text_color = "white"
+        textLED1.text_color = "green"
     else:
-        digitalWrite(myLED, 0)
-        ledButton.text = "Turn ON"
-        ledButton.bg = "red"
-        textLED.color = "black"
+        digitalWrite(myLED1, 0)
+        ledButton1.text = "Turn ON"
+        ledButton1.bg = "red"
+        textLED1.color = "black"
+
+def ledToggle2():
+    button_status = digitalRead(myLED2)
+    if (button_status == 0):
+        digitalWrite(myLED2, 1)
+        ledButton2.text = "Turn OFF"
+        ledButton2.bg = "green"
+        ledButton2.text_color = "white"
+        textLED2.text_color = "green"
+    else:
+        digitalWrite(myLED2, 0)
+        ledButton2.text = "Turn ON"
+        ledButton2.bg = "red"
+        textLED2.color = "black"
+
+def ledToggle3():
+    button_status = digitalRead(myLED3)
+    if (button_status == 0):
+        digitalWrite(myLED3, 1)
+        ledButton3.text = "Turn OFF"
+        ledButton3.bg = "green"
+        ledButton3.text_color = "white"
+        textLED3.text_color = "green"
+    else:
+        digitalWrite(myLED3, 0)
+        ledButton3.text = "Turn ON"
+        ledButton3.bg = "red"
+        textLED3.color = "black"
 
 app = App(title="TCSS573: IoT Activity 02", height=300, width=500)
 main = Text(app, text="LED Fading", size=14, font="Times New Roman", color="navy")
 box = Box(app, layout="grid", grid=[1, 0])
 
 textLED1 = Text(box, text="BLUE", align="left", grid=[0, 0])
-ledButton1 = PushButton(box, command=ledToggle(myLED1, ledButton1, textLED1), text="Turn ON", grid=[1, 0])
-sliderLED1 = Slider(box, start=0, end=255, command=sliderToggleLED(myLED1), grid=[4, 0])
+ledButton1 = PushButton(box, command=ledToggle1, text="Turn ON", grid=[1, 0])
+sliderLED1 = Slider(box, start=0, end=255, command=sliderToggleLED1, grid=[4, 0])
 
-textLED2 = Text(box, text="GREEN", align="left", grid=[0, 0])
-ledButton2 = PushButton(box, command=ledToggle(myLED2, ledButton2, textLED2), text="Turn ON", grid=[1, 0])
-sliderLED2 = Slider(box, start=0, end=255, command=sliderToggleLED(myLED2), grid=[4, 0])
+textLED2 = Text(box, text="GREEN", align="left", grid=[0, 8])
+ledButton2 = PushButton(box, command=ledToggle2, text="Turn ON", grid=[1, 8])
+sliderLED2 = Slider(box, start=0, end=255, command=sliderToggleLED2, grid=[4, 8])
 
-textLED3 = Text(box, text="RED", align="left", grid=[0, 0])
-ledButton3 = PushButton(box, command=ledToggle(myLED3, ledButton3, textLED3), text="Turn ON", grid=[1, 0])
-sliderLED3 = Slider(box, start=0, end=255, command=sliderToggleLED(myLED3), grid=[4, 0])
+textLED3 = Text(box, text="RED", align="left", grid=[0, 12])
+ledButton3 = PushButton(box, command=ledToggle3, text="Turn ON", grid=[1, 12])
+sliderLED3 = Slider(box, start=0, end=255, command=sliderToggleLED3, grid=[4, 12])
 
 
 
-exitButton = PushButton(box, command=exitProgram, text="Exit", grid=[2, 8])
+exitButton = PushButton(box, command=exitProgram, text="Exit", grid=[2, 18])
 app.display()
 
